712c1c14b17669f28983e8a5394d9984623cb243
AJ Fite <austinjfite@hotmail.com>
2012-04-10 21:07:24 -0700

    Comments and styles

--- public/Assets/Scripts/Pages/rules.js
+++ public/Assets/Scripts/Pages/rules.js
@@ -12,10 +12,12 @@ function show(sec) {
     var item = document.getElementById('Section'+sec);
     var ol = item.getElementsByTagName('ol')[0];
     if(ol.style.display != "inherit") {
+	//Make things visible
 	ol.style.display = "inherit";
 	ol.style.visibility = "visible";
 	item.getElementsByTagName('img')[0].src = "Assets/Images/Shrink.png";
     } else {
+	//Make things hidden
 	ol.style.display = "none";
 	ol.style.visibility = "collapse";
 	item.getElementsByTagName('img')[0].src = "Assets/Images/Expand.png";
