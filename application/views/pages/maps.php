a38aa37e1390d01a87562e42fd0979d9493f64fd
AJ Fite <austinjfite@hotmail.com>
2012-04-26 21:23:23 -0700

    Rail map now live

--- application/views/pages/maps.php
+++ application/views/pages/maps.php
@@ -5,6 +5,7 @@
 			<dd>A dynamic live map with realtime rendering and player location updates along with extremely high definition renders.</dd>
 			<dd><a href="/dynmap">View the Dynmap</a></dd>
 		<dt>Minecart System</dt>
-			<dd>A static map of the MTA rail system. (coming soon)</dd>
+			<dd>A static map of the MTA rail system.</dd>
+			<dd><a href="/Assets/Images/RailSystem.png">View the Rail Map</a></dd>
 	</dl>
 </div>
