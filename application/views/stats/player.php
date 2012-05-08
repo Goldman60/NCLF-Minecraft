c2c8866b309fd4e033a957e2ba7c8bef06a63036
AJ Fite <austinjfite@hotmail.com>
2012-05-07 17:18:06 -0700

    Further work on the stats page and some site styling

--- application/views/stats/player.php
+++ application/views/stats/player.php
@@ -1,15 +1,19 @@
 <div id="full-body">
 	<h2><img class="playerFace" src="/dynmap/tiles/faces/32x32/<?php echo $playerData->getName(); ?>.png" alt="<?php echo $playerData->getName(); ?>'s Player Face" title="<?php echo $playerData->getName(); ?>'s Player Face" /> <?php echo $playerData->getName(); ?>'s Stats</h2>
 	<p class="SubStats">Since April 8, 2012 on the main NCLF Server</p>
-	<?php if($playerData->isOnline()) { ?><div class="online">Online</div><?php } else { ?><div class="offline">Offline, last seen  <?php echo date(DATE_TIME, $playerData->getLastLogin()); ?></div><?php } ?>
-	<div id="LoginStats">First Login: <?php echo date(DATE_TIME, $playerData->getFirstLogin()); ?> | Total Time Online: <?php echo $playerPre['online']; ?></div>
+	<?php if($playerData->isOnline()) { ?><div class="online">Online, since <?php echo date(DATE_TIME,$playerData->getCurrentLoginTime()); ?></div><?php } else { ?><div class="offline">Offline, last seen  <?php echo date(DATE_TIME, $playerData->getLastLogin()); ?></div><?php } ?>
+	<div id="LoginStats">
+		<ul>
+			<li>First Login: <?php echo date(DATE_TIME, $playerData->getFirstLogin()); ?></li>
+			<li>Total Time Online: <?php echo $playerPre['online']; ?></li>
+			<li>Total Logins: <?php echo $playerData->getNumberOfLogins(); ?></li>
+		</ul>
+	</div>
 	
 	<div id="DistanceTravelled">
+	<h3>Distance Travelled</h3>
 		<table>
 			<thead>
-				<tr>
-					<th colspan="2">Distance Travelled</th>
-				</tr>
 				<tr>
 					<th>Mode</th>
 					<th>Distance (Meters)</th>
@@ -41,4 +45,44 @@
 			</tfoot>
 		</table>
 	</div> 
+	
+	<?php 
+		$placed = $playerData->getBlocksMostPlaced(); 
+		$destroy = $playerData->getBlocksMostDestroyed(); 
+		$blockTable = $playerData->getPlayerBlockTable();
+		
+		//Remove air from the block table
+		
+	?>
+	
+	<div id="Blocks">
+		<h3>Block Quick Stats</h3>
+		<ul>
+			<li>Total Blocks Destroyed: <?php echo $playerData->getBlocksDestroyedTotal(); ?></li>
+			<li>Most Destroyed Block: <?php echo Stats_utilities::getResourceNameById($destroy['block_id']); ?> (<?php echo $destroy['sum']; ?>)</li>
+			<li>Total Blocks Placed: <?php echo $playerData->getBlocksPlacedTotal(); ?></li>
+			<li>Most Placed Block: <?php echo Stats_utilities::getResourceNameById($placed['block_id']); ?> (<?php echo $placed['sum']; ?>)</li>
+		</ul>
+	</div>
+	<div id="DetailedBlock">
+		<h3>Detailed Block Breakdown</h3> 
+		<table>
+			<thead>
+				<tr>
+					<th>Block Type</th>
+					<th>Placed</th>
+					<th>Destroyed</th>
+				</tr>
+			</thead>
+			<tbody>
+			<?php foreach ($blockTable as $block): ?>
+				<tr>
+					<td><?php echo Stats_utilities::getResourceNameById($block["block_id"]); ?></td>
+					<td><?php echo $block["num_placed"]; ?></td>
+					<td><?php echo $block["num_destroyed"]; ?></td>
+				</tr>
+			<?php endforeach; ?>
+			</tbody>
+		</table>
+	</div>
 </div>
